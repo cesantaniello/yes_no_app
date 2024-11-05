14d2433f8ed83d0d86ed47a91b69dd3191a99115
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-11-05 17:09:09 +0100

    Create ListView

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -10,11 +10,36 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/588353819312054274/1FN4L8aG_400x400.jpg'),
+            backgroundImage: NetworkImage(
+                'https://pbs.twimg.com/profile_images/588353819312054274/1FN4L8aG_400x400.jpg'),
           ),
         ),
         title: const Text('Hola'),
         centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              }),
+            ),
+            const Text('Mundo'),
+          ],
+        ),
       ),
     );
   }
