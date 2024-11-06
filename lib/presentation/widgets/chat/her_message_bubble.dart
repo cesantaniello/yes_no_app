a418bcc517e4769bd317430307d4ecce800936ca
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-11-06 17:00:07 +0100

    Show message when image is loading

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -42,7 +42,16 @@ class _ImageBubble extends StatelessWidget {
           width: size.width * 0.7,
           height: 150,
           fit: BoxFit.cover,
-        )
-    );
+          loadingBuilder: (context, child, loadingProgres) {
+            if (loadingProgres == null) return child;
+
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('Hola, qué hace?'),
+            );
+          },
+        ));
   }
 }
