07792cba9db7d08bb1ee97c2b630f9b15f2a62ce
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-12-15 15:09:28 +0100

    Add messages to provider

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
@@ -9,32 +10,32 @@ class MessageFieldBox extends StatelessWidget {
     final focusNode = FocusNode();
 
     final outlineInputBorder = UnderlineInputBorder(
-      borderSide: const BorderSide(color: Colors.transparent),
-      borderRadius: BorderRadius.circular(40)
-    );
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
-      hintText: 'End your message with a "?"',
-      enabledBorder: outlineInputBorder,
-      focusedBorder: outlineInputBorder,
-      filled: true,
-      suffixIcon: IconButton(
-        onPressed: () {
-          final textValue = textController.value.text;
-        },
-        icon: const Icon(Icons.send_outlined),
-      )
-    );
+        hintText: 'End your message with a "?"',
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          onPressed: () {
+            final textValue = textController.value.text;
+            textController.clear();
+            onValue(textValue);
+          },
+          icon: const Icon(Icons.send_outlined),
+        ));
 
     return TextFormField(
-      onTapOutside: (event) => focusNode.unfocus(),
-      focusNode: focusNode,
-      controller: textController,
-      decoration: inputDecoration,
-      onFieldSubmitted: (value) {
-        textController.clear();
-        focusNode.requestFocus();
-      }
-    );
+        onTapOutside: (event) => focusNode.unfocus(),
+        focusNode: focusNode,
+        controller: textController,
+        decoration: inputDecoration,
+        onFieldSubmitted: (value) {
+          textController.clear();
+          focusNode.requestFocus();
+          onValue(value);
+        });
   }
 }
